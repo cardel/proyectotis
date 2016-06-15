e2c74c217ea47ee473eb3984a627d2be2d4f8f35
Carlos Delgado <cardel87@gmail.com>
2016-06-15 11:45:21 +0000

    Aplicacion tenant

--- proyectotis/public_urls.py
+++ proyectotis/public_urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from productortenant.views import ProductorForm
 #Urls publicas
 urlpatterns = [
-   url(r'^$', ProductorForm.as_view(), name='registrarProductor'),
+   url(r'^$', ProductorForm.as_view()),
 
 ]
