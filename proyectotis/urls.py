eb0f9309e70178f3f5a67d27db48d03628ae61eb
Carlos Delgado <cardel87@gmail.com>
2016-06-15 10:55:16 +0000

    Aplicacion tenant

--- proyectotis/urls.py
+++ proyectotis/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 
-import gestionfruta
+import productortenant
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
