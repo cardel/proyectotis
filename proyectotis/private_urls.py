7f3120a5fddb8ccce066c57be5f655b798563839
Carlos Delgado <cardel87@gmail.com>
2016-06-15 13:23:30 +0000

    Aplicacion tenant

--- proyectotis/private_urls.py
+++ proyectotis/private_urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
 
 
-from gestionfruta import views
+import gestionfruta
 urlpatterns = [
 
-   url(r'^$', 'views.inicio', name='inicio'),
+   url(r'^$', 'gestionfruta.views.inicio', name='inicio'),
 
 ]
 
