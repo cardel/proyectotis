a8d0bb41008121488670d3d9a56b96d471a1d691
Carlos Delgado <cardel87@gmail.com>
2016-06-15 10:58:18 +0000

    Aplicacion tenant

--- proyectotis/urls.py
+++ proyectotis/urls.py
@@ -17,10 +17,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 
-import productortenant
+import gestionfruta
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'productortenant.views.registrarProductor', name='registrarProductor'),
+   url(r'^$', 'gestionfruta.views.inicio', name='inicio'),
 
 ]
