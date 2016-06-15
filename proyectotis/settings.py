c9c6617066472adb4dbc14193cec0940f50ec29f
Carlos Delgado <cardel87@gmail.com>
2016-06-15 14:39:01 +0000

    Aplicacion tenant

--- proyectotis/settings.py
+++ proyectotis/settings.py
@@ -88,16 +88,13 @@ TEMPLATES = [
         'OPTIONS': {
             'debug': DEBUG,
 
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+
         },
     },
 ]
-
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+)
 
 WSGI_APPLICATION = 'proyectotis.wsgi.application'
 
