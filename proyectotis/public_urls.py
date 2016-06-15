af5b3478b7704ba5454d6c6f876f958f685d5355
Carlos Delgado <cardel87@gmail.com>
2016-06-15 14:25:02 +0000

    Aplicacion tenant

--- proyectotis/public_urls.py
+++ proyectotis/public_urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from productortenant.views import ProductorForm
+from productortenant.views import ProductorForm, HomeView
 from django.contrib import admin
 
 #Urls publicas
 urlpatterns = [
-    url(r'^$', ProductorForm.as_view()),
+    url(r'^crea$', ProductorForm.as_view()),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view()),
 ]
